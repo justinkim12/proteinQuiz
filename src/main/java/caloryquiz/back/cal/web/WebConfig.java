5b7b6124085bbbd85ee450223230b6cfa7d7bbac
kim <justinkim1201@gmail.com>
2023-07-17 00:10:54 +0900

    CORS이슈반영

--- src/main/java/caloryquiz/back/cal/web/WebConfig.java
+++ src/main/java/caloryquiz/back/cal/web/WebConfig.java
@@ -28,8 +28,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://port-0-proteinquiz-20zynm2mlk2a7j4g.sel4.cloudtype.app/") // 허용할 출처
-                .allowedMethods("GET", "POST") // 허용할 HTTP method
+                .allowedOrigins("https://protein-quiz.vercel.app") // 허용할 출처
+                .allowedMethods("OPTIONS","GET", "POST") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
